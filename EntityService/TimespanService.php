2e36c4f0370b6c82fcec9647cc97ef1fb3e4c2fb
Sandro Groganz <sandro@campaignchain.com>
2016-10-29 11:24:03 +0200

    CampaignChain/campaignchain#263 Use getManager() instead of getEntityManager() for Doctrine

--- EntityService/TimespanService.php
+++ EntityService/TimespanService.php
@@ -20,7 +20,7 @@ namespace CampaignChain\Hook\TimespanBundle\EntityService;
 use CampaignChain\CoreBundle\Entity\Hook;
 use CampaignChain\CoreBundle\EntityService\HookServiceTriggerInterface;
 use CampaignChain\Hook\TimespanBundle\Entity\Timespan;
-use Doctrine\ORM\EntityManager;
+use Doctrine\Common\Persistence\ManagerRegistry;
 use Doctrine\Common\Inflector\Inflector;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -29,10 +29,10 @@ class TimespanService implements HookServiceTriggerInterface
     protected $em;
     protected $container;
 
-    public function __construct(EntityManager $em, ContainerInterface $container)
+    public function __construct(ManagerRegistry $managerRegistry, ContainerInterface $container)
     {
         $this->container = $container;
-        $this->em = $em;
+        $this->em = $managerRegistry->getManager();
     }
 
     public function getHook($entity, $mode = Hook::MODE_DEFAULT){
